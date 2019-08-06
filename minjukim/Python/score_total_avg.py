c129ecfc790e05ec53b09dc4016b2491364d4266
kim_min_ju <kim4pb@gmail.com>
2019-08-06 10:22:57 +0900

    modified by Ashal's feedback

--- minjukim/Python/score_total_avg.py
+++ minjukim/Python/score_total_avg.py
@@ -1,14 +1,11 @@
-def total(list):
+def total(l):
     total_score = 0
-    for score in list:
+    for score in l:
         total_score += score
     return total_score
 
-def average(list):
-    total_score = 0
-    for score in list:
-        total_score += score
-    avg_score = total_score / len(list)
+def average(l):
+    avg_score = total(l) / len(l)
     return avg_score
 
 scores = [80, 100, 70, 90, 40]
