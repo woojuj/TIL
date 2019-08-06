0367023230dca10bfb07b8f9e7f8411a57241a29
Bokyung <alicebk221@gmail.com>
2019-08-06 21:52:59 +0900

    edit def_sum

--- bokyungjung/python/def_sum.py
+++ bokyungjung/python/def_sum.py
@@ -1,28 +1,27 @@
 scores = [80, 100, 70, 90, 40]
 
 # 총합 구하기
-def total(list):
+def total(scores):
     total_score=0
-    for i in range(0, len(list)):
-        total_score += list[i]
+    for i in range(0, len(scores)):
+        total_score += scores[i]
     return total_score
 
 print(total(scores))
 
 # total 함수 다시 활용하기
 
-def average(list):
-    return total(list)/len(list)
+def average(scores):
+    return total(scores)/len(scores)
 
 print(average(scores))
 
 # total 함수 활용하지 않고 Average 함수 만들기
-'''
-def average(list):
+
+def average(scores):
     total_score = 0
-    for i in range(0, len(list)):
-        total_score += list[i]
-    return total_score/len(list)
+    for i in range(0, len(scores)):
+        total_score += scores[i]
+    return total_score/len(scores)
 
 print(average(scores))
-'''
