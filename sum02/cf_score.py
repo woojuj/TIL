875e19c65a0fcb08faec3e2ebdfa84d4b0eb1080
sum02 <tnaud2@gmail.com>
2019-08-06 15:30:42 +0900

    Third Commit: Modify avg

--- sum02/cf_score.py
+++ sum02/cf_score.py
@@ -14,3 +14,14 @@ def avg(scores):
     total_avg = total_sum / len(scores)
     return total_avg
 print(avg(scores))
+
+"""
+190806 
+total 함수를 호출하는 avg2 함수 만들기
+더 훌륭한 함수가 되었어요!!!
+"""
+
+def avg2(scores):
+    avg2 = total(scores) / len(scores)
+    return avg2
+print(avg2(scores))
