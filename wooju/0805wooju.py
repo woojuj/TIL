9648a548f93c46e7b6d3e862982fbbafbc0cdcc3
dubu <dubu@dubuui-MacBook-Air.local>
2019-08-09 18:05:13 +0900

    python0805

--- wooju/0805wooju.py
+++ wooju/0805wooju.py
@@ -1,19 +1,15 @@
-
-def total(scores):
+def chong(scores):
     acc = 0
     for i in range(0, len(scores)):
         acc += scores[i]
     return acc
 
-print(total([80, 100, 70, 90, 40]))
+
+print(chong([80, 100, 70, 90, 40]))
 
 
 def pyeong(scores):
-    acc = 0
-    for i in range(0, len(scores)):
-        acc += scores[i]
-    return acc / len(scores)
+    return chong(scores) / len(scores)
+
 
 print(pyeong([80, 100, 70, 90, 40]))
-
-# 1234
