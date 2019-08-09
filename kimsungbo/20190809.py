f1bc69de1cf63dd371f2a4084e85c683c8af239d
kimsungbo <kimsungbo97@gmail.com>
2019-08-09 17:35:36 +0900

    python assertion

--- kimsungbo/20190809.py
+++ kimsungbo/20190809.py
@@ -1,6 +1,9 @@
 def test_simple():
     assert total([1, 2, 3]) == 1 + 2 + 3
+    assert total([1, 2, 3, 4, 5]) == 1 + 2 + 3 + 4 + 5
+    assert total([1, 1, 1, 1, 1]) == 1 + 1 + 1 + 1 + 1
     assert average([2, 4, 6]) == (2 + 4 + 6) / 3
+    assert average([2, 4, 6, 8, 10]) == (2 + 4 + 6 + 8 + 10) / 5
 
 
 scores = [80, 100, 70, 90, 40]
