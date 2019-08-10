0939983be72c75dcfc44e150ee985d8a6dbe8e84
Ashal aka JOKER <ahastudio@gmail.com>
2019-08-10 12:18:21 +0900

    Take code from pull request #45
    Make some changes with code written on the pull request page.
    
    Code from https://github.com/dataitgirls3/TIL/pull/45

--- hyejeongsong/190805_p1.py
+++ hyejeongsong/190805_p1.py
@@ -1,10 +1,14 @@
 scores = [80,100,70,90,40]
 
-def total_score():
-    return sum(scores)
+def total_score(scores):
+    total_score = 0
+    for i in range(0,len(scores)):
+        total_score += scores[i]
+    return total_score
 
-print("Total score:", total_score())
+print("Total score:", total_score(scores))
 
-def avg_score():
-    return total_score() / len(scores)
-print("Average score:", avg_score())
+def avg_score(scores):
+    return total_score(scores) / len(scores)
+
+print("Average score:", avg_score(scores))
